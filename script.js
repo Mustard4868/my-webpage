7a08bff57ea4927853f73096b4bc72cbe6fc21fd
Mustard <102016032+Mustard4868@users.noreply.github.com>
2024-04-29 22:36:16 +0200

    add new page and fix dropdown for phones

---
+++ script.js
@@ -0,0 +1,8 @@
+function clickMenu() {
+    var x = document.getElementsByClassName("w3-dropdown-content")[0];
+    if (x.className.indexOf("w3-show") == -1) {
+        x.className += " w3-show";
+    } else {
+        x.className = x.className.replace(" w3-show", "");
+    }
+}
